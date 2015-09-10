2bf739f19054cdc680eb6735f28aef184c03647e
jamal senouci <jamalsenouci@gmail.com>
2015-09-10 17:49:01 +0100

    initial commit

---
+++ context.bat
@@ -0,0 +1,10 @@
+@echo off
+SET st3Path=C:\Users\jamal.senouci\Google Drive\Tech Dev\Software\Sublime Text 3\sublime_text.exe
+
+ 
+rem add it for all file types
+@reg add "HKEY_CLASSES_ROOT\*\shell\Open with Sublime Text 3"         /t REG_SZ /v "" /d "Open with Sublime Text 3"   /f
+@reg add "HKEY_CLASSES_ROOT\*\shell\Open with Sublime Text 3"         /t REG_EXPAND_SZ /v "Icon" /d "%st3Path%,0" /f
+@reg add "HKEY_CLASSES_ROOT\*\shell\Open with Sublime Text 3\command" /t REG_SZ /v "" /d "%st3Path% \"%%1\"" /f
+
+pause
